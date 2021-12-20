2fe5402cd736bf97941a6528246a6397120b017f
Alaska01 <aadaniel108@gmail.com>
2021-12-20 01:22:55 +0100

    Add password field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_232449) do
+ActiveRecord::Schema.define(version: 2021_12_20_002201) do
 
   create_table "favourites", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_12_19_232449) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
   add_foreign_key "favourites", "houses"

aa4cac159bd41a7c72066edd6aceab2c00f00720
Alaska01 <aadaniel108@gmail.com>
2022-01-02 11:48:01 +0100

    Fixed routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      root 'houses#index'
+      # root 'houses#index'
       resources :users     
       post 'signup', to: 'users#create'
       post 'login', to: 'authentication#create'

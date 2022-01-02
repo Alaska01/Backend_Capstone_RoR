89826204cb8dd227424bdd4e7e22d4d93cf9f9bc
Alaska01 <aadaniel108@gmail.com>
2022-01-02 12:01:16 +0100

    Delete comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      # root 'houses#index'
       resources :users     
       post 'signup', to: 'users#create'
       post 'login', to: 'authentication#create'

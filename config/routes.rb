2e2f2a93cee015ff37fef048002e065914487b11
Alaska01 <aadaniel108@gmail.com>
2022-01-02 11:46:06 +0100

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      #add a root resource here later
-      # post "/login", to: "users#login"
-      # get "/auto_login", to: "users#auto_login"
-      resources :users
-      # resources :houses
-      # resources :favourites
-      # get '/users/:id', to: 'users#import1'
       root 'houses#index'
+      resources :users     
       post 'signup', to: 'users#create'
       post 'login', to: 'authentication#create'
       resources :houses, only: %i[index show]
